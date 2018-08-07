45acc4321cb8496f2ab429135263bd4550648ed3
Jim Huang <jserv.tw@gmail.com>
2018-08-07 14:33:13 +0800

    Mention external source files
    nstack utilizes parts of FreeBSD libraries, which are freely
    redistributable under the two-clause BSD License.

--- src/collection.h
+++ src/collection.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991, 1993
- *        The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 1991, 1993 The Regents of the University of California.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -10,9 +10,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
