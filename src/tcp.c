f4d6053c1ab77dd1d3fe9457cf7ca43fb8303ae7
bauuuu1021 <bboy010554@icloud.com>
2019-04-24 23:08:33 +0800

    TCP: Add Linux kernel-based random number generator (#7)
    Besides POSIX srand/rand, this patch introduced /dev/urandom as RNG.

--- src/tcp.c
+++ src/tcp.c
@@ -1,7 +1,14 @@
+#if defined(__linux__)
+#include <fcntl.h>
+#include <linux/random.h>
+#else
+#include <time.h>
+#endif
+
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <unistd.h>
 
 #include "nstack_ip.h"
 #include "nstack_socket.h"
@@ -246,8 +253,15 @@ static int tcp_fsm(struct tcp_conn_tcb *conn,
             }
             rs->tcp_flags |= TCP_ACK;
             rs->tcp_ack_num = rs->tcp_seqno + 1;
+
+#if defined(__linux__)
+            int fd = open("/dev/urandom", O_RDONLY);
+            read(fd, &(rs->tcp_seqno), sizeof(rs->tcp_seqno));
+            close(fd);
+#else
             srand(time(NULL));
             rs->tcp_seqno = rand() % 100;
+#endif
 
             if (sock) {
                 conn->state = TCP_SYN_RCVD;
